888355f0a23f50ed71ad5d7fa62fd3869dda864b
Lauren Black <laurenblack5280@gmail.com>
2022-04-14 17:21:14 -0600

    Seperate the dom from the data model

--- idea.js
+++ idea.js
@@ -6,12 +6,6 @@ class Idea() {
     this.star = false;
   }
 
-// saveToStorage(){
-//
-// }
-// deleteFromStorage(){
-//
-// }
 updateIdea(){
   console.log("initial value", this.star)
 if (this.star === false) {
@@ -21,14 +15,3 @@ if (this.star === false) {
 }
 
 }
-
-var ideaBoxDetails = [
-{
-  id: Date.now()
-  title: "Idea title";
-  body: "Idea body. Don't ever play yourself. Every chance I get, I water the plants, Lion! Cloth talk.";
-  star: false;
-}
-]
-
-var ideaBoxDetails = document.querySelector()
---
+++ main.js
@@ -0,0 +1,29 @@
+var cardDetails = [
+{
+  id: Date.now();
+  title: "Idea title";
+  body: "Idea body. Don't ever play yourself. Every chance I get, I water the plants, Lion! Cloth talk.";
+  star: false;
+}
+]
+var cardDetails = document.querySelectorAll(".box")
+// elementList = parentNode.querySelectorAll(selectors)
+
+var ideas = []
+
+//eventlisteners here
+
+
+
+
+
+
+
+//functions here
+function pushInfo(userImgURL, userNewTitle, userNewQuote) {
+  currentCard = new Idea(id, title, body, star)
+  id.push(newId.value)
+  title.push(newTitle.value)
+  body.push(newBody.value)
+  showHide(cardForm, mainPost)
+}
