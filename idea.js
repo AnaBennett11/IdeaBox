7c411206026f6da85fe9a96bba06cb88512ff155
Cleveland Ticoalu <ayocleveland@gmail.com>
2022-04-18 14:04:07 -0600

    Refactor all files for completion

--- idea.js
+++ idea.js
@@ -4,13 +4,5 @@ class Idea {
     this.title = title;
     this.body = body;
     this.star = false;
-}
   }
-// updateIdea() {
-//   // console.log("initial value", this.star)
-// if (this.star === false) {
-//   this.star = true
-//   // console.log("click", this.star)
-//     }
-//   }
-// }
+}
--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 var cardDetails = document.querySelectorAll(".box");
 var saveButton = document.querySelector(".save");
 var titleInput = document.querySelector("#title-id");
@@ -6,11 +5,7 @@ var bodyInput = document.querySelector("#body-id");
 var wrapper = document.querySelector(".wrapper");
 var deleteButton = document.querySelector("delete");
 
-// elementList = parentNode.querySelectorAll(selectors)
-
 var ideas = [];
-
-//eventlisteners here
 
 saveButton.addEventListener("click", function(event) {
   event.preventDefault();
@@ -22,72 +17,43 @@ saveButton.addEventListener("click", function(event) {
 titleInput.addEventListener("keyup", keyupFunction)
 bodyInput.addEventListener("keyup", keyupFunction)
 
- wrapper.addEventListener("click", function(event) {
-   // console.log(event.target.id);
-   // console.log(event.target.nodeName);
-   // console.log(event.target.classList.contains('button-favorite'));
-   // console.log(event.target.classList.contains('button-delete'));
-   // //removeIdea(event);
-   //showStar(event);
-   // if(nodeName === img && the class === buttonFavorite) {
-   //   console.log("this is a favorite click");
-   // } else if (nodeName === img && the class === buttonDelete){
-   //   console.log("remove idea");
-   // }
-     if (event.target.nodeName.toLowerCase() == "img" && event.target.classList.contains("button-favorite")) {
-       // console.log("favorite click");
+wrapper.addEventListener("click", function(event) {
+      if (event.target.nodeName.toLowerCase() == "img" && event.target.classList.contains("button-favorite")) {
         toggleFavorite(event);
-     } else if (event.target.nodeName.toLowerCase() == "img" && event.target.classList.contains("button-delete")) {
-        removeIdea(event); // console.log("delete click");
-     }
- })
-
+      } else if (event.target.nodeName.toLowerCase() == "img" && event.target.classList.contains("button-delete")) {
+        removeIdea(event);
+  }
+})
 
 function toggleFavorite(event) {
-//update the database with the users favorite preferences for selected card
-//redisplay the cards
   for (var i = 0; i < ideas.length; i++) {
     if (`star${ideas[i].id}` === event.target.id) {
       console.log("the current favorite value is " + ideas[i].star);
       if (ideas[i].star === true) {
         ideas[i].star = false;
-      }else{
+      } else {
         ideas[i].star = true;
       }
-     }
-
     }
+  }
 displayCard();
-
 }
-
-
-//functions go here
 
 function pushCard () {
 var idea = new Idea (titleInput.value, bodyInput.value)
 ideas.push(idea);
-console.log(ideas);
-  }
+}
 
 function displayCard () {
-
-  var imageSRC = "";
-
-
-
-  wrapper.innerHTML = "";
-  //i'm gonna create a for loop inside of this array to pull the title and body
+var imageSRC = "";
+wrapper.innerHTML = "";
   for(var i = 0; i < ideas.length; i++) {
-
-    //  determine value of imageSRC.
     if (ideas[i].star === true) {
       imageSRC = "star-active.svg";
-    }else{
+    } else {
       imageSRC = "star.svg";
-    }
-
-    wrapper.innerHTML += `
+}
+wrapper.innerHTML += `
     <box class="box">
       <section class="top-bar">
         <button class="top-card-button"><img class="card-img button-favorite" id="star${ideas[i].id}" src="assets/${imageSRC}" alt="color-star">
@@ -108,9 +74,6 @@ function displayCard () {
   }
 }
 
-
-
-
 function clearInputs() {
 titleInput.value = null;
 bodyInput.value = null;
@@ -118,24 +81,21 @@ keyupFunction();
 }
 
 function keyupFunction(event) {
-  if (titleInput.value && bodyInput.value){
+  if (titleInput.value && bodyInput.value) {
     saveButton.disabled = false;
-  }else{
+  } else {
     saveButton.disabled = true;
   }
 }
 
 function removeIdea(event) {
    id = (event.target.id);
-   // ideas = ideas.filter(ideas => ideas.id !== id);
    for (var i = 0; i < ideas.length; i++) {
      if (`${ideas[i].id}` === id) {
      ideas.splice(i, 1)
-   }
- }
-   // above line equals for each index in ideas, check if that id equals
-   // event id.
-   displayCard();
+    }
+  }
+  displayCard();
 }
 
 function showStar(event) {
@@ -144,14 +104,6 @@ function showStar(event) {
   for (var i = 0; i < ideas.length; i++) {
     if (`star${ideas[i].id}` === id) {
       document.getElementById(id).src ="assets/star.svg";
-
+    }
   }
 }
-}
-// As a user,
-//
-// When I click the “Star” button on an idea card,
-// The card instance’s star property should be updated in the ideas array
-// When the button was an outline of a star (not favorited), the button should now be a filled in star (favorited)
-// and vice versa (for unfavoriting)
-//
